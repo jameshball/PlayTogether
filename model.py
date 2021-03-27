8aaf5c2d0a29aa375e7b10475d3ca8a5aacbc3e5
Andy Wang <cbeuw.andy@gmail.com>
2021-03-27 16:44:24 +0000

    Update relationship model

--- model.py
+++ model.py
@@ -2,8 +2,8 @@ from index import db
 
 
 class Score(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80), nullable=False)
     tracks = db.relationship('Track', backref='score')
 
     def __repr__(self):
@@ -14,12 +14,11 @@ class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
-    bars = db.relationship('Bar', backref='track')
 
 
 class Bar(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tempo = db.Column(db.Integer, nullable=False)
     timesig_top = db.Column(db.Integer, nullable=True)
-    track_id = db.Column(db.Integer, db.ForeignKey('track.id'), nullable=False)
+    score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
 
