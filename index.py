6adc5cc0d04ed1fd8ffb29ec0110b4a5b1d03bbc
Andy Wang <cbeuw.andy@gmail.com>
2021-03-27 14:41:17 +0000

    Remove DB processing from index.py

--- index.py
+++ index.py
@@ -1,11 +1,7 @@
-import os
 from flask import Flask, send_file
-from flask_sqlalchemy import SQLAlchemy
 from pydub import AudioSegment
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
 
 
 @app.route('/')
