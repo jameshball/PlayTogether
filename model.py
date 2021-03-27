e44a36c18f9609f5f5c55c55d4a982b997512901
Andy Wang <cbeuw.andy@gmail.com>
2021-03-27 17:18:56 +0000

    Update relationship model

--- model.py
+++ model.py
@@ -1,19 +1,16 @@
 from index import db
+from sqlalchemy.sql import func
 
 
 class Score(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
+
     tracks = db.relationship('Track', backref='score')
+    bars = db.relationship('Bar', backref='score')
 
     def __repr__(self):
         return f"<Score {self.name}>"
-
-
-class Track(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
 
 
 class Bar(db.Model):
@@ -22,3 +19,18 @@ class Bar(db.Model):
     timesig_top = db.Column(db.Integer, nullable=True)
     score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
 
+
+class Track(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    score_id = db.Column(db.Integer, db.ForeignKey('score.id'), nullable=False)
+
+    samples = db.relationship('Sample', backref='track')
+
+
+class Sample(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file = db.Column(db.LargeBinary)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+
+    track_id = db.Column(db.Integer, db.ForeignKey('track.id'), nullable=False)
