8552f4474e38605179115988260e126ca8fcf7bd
Jessica Lally <jml19@ic.ac.uk>
2021-03-28 02:48:57 +0100

    Implement checkbox list for backing tracks

--- static/record_track.js
+++ static/record_track.js
@@ -1,40 +1,92 @@
-let dropdown = document.getElementById('sampling-track-dropdown')
-dropdown.length = 0
-
-let defaultOption = document.createElement('option')
-defaultOption.text = 'Choose sampling track'
-
-dropdown.add(defaultOption)
-dropdown.selectedIndex = 0
-
-// Open GET request to get tracks for score
-const track_url = '/api/list_tracks/' + score_id
-const request = new XMLHttpRequest()
-request.responseType = "json";
-request.open('GET', track_url, true)
-
-request.onload = function () {
-    if (request.status === 200) {
-        const tracks_data = request.response
-        console.log(tracks_data)
-        let track
-
-        for (let i=0; i < tracks_data.length; i++) {
-            track = document.createElement('option')
-            track.text = tracks_data[i].name
-            track.value = tracks_data[i].track_id
-            dropdown.add(track)
-        }
-    } else {
-        console.error('An error occurred fetching the track options')
-    }
-}
-
-request.send()
-
 let shouldStop = false;
 let stopped = false;
-let mediaRecorder = null
+let mediaRecorder = null;
+
+function getSamplingTracks() {
+    let dropdown = document.getElementById('sampling-track-dropdown')
+    dropdown.length = 0
+
+    let defaultOption = document.createElement('option')
+    defaultOption.text = 'Choose sampling track'
+    defaultOption.value = '0'
+
+    dropdown.add(defaultOption)
+    dropdown.selectedIndex = 0
+
+    let backing_tracks = document.getElementById('backing-track-options')
+
+    // Open GET request to get tracks for score
+    const track_url = '/api/list_tracks/' + score_id
+    const request = new XMLHttpRequest()
+    request.responseType = "json";
+    request.open('GET', track_url, true)
+
+    request.onload = function () {
+        if (request.status === 200) {
+            const track_data = request.response
+            let track
+
+            for (let i = 0; i < track_data.length; i++) {
+                track = document.createElement('option')
+                track.text = track_data[i].name
+                track.value = track_data[i].track_id
+                dropdown.add(track)
+
+                // Open GET request to get backing track samples for each track_id
+                for (let i = 0; i < track_data.length; i++) {
+                    const box = document.createElement('div')
+                    // Create checkbox for track_id
+                    let checkbox = document.createElement('input')
+                    checkbox.type = "checkbox"
+                    checkbox.text = track_data[i].name
+                    checkbox.value = track_data[i].track_id
+                    checkbox.id = "id"
+
+                    let sample_dropdown = document.createElement('select')
+                    sample_dropdown.length = 0
+
+                    let defaultOption = document.createElement('option')
+                    defaultOption.text = 'Choose backing track'
+                    defaultOption.value = '0'
+
+                    sample_dropdown.add(defaultOption)
+                    sample_dropdown.selectedIndex = 0
+
+                    const samples_url = '/api/list_samples/' + track_data[i].track_id
+                    const request = new XMLHttpRequest()
+                    request.responseType = "json";
+                    request.open('GET', samples_url, true)
+
+                    request.onload = function () {
+                        if (request.status === 200) {
+                            samples = request.response
+
+                            for (let i = 0; i < samples.length; i++) {
+                                option = document.createElement('option')
+                                option.text = samples[i].track_id
+                                option.value = track_data[i].date
+                                sample_dropdown.add(option)
+                            }
+                        } else {
+                            console.error('An error occurred fetching the sample options')
+                        }
+                    }
+
+                    request.send()
+
+                    box.appendChild(document.createTextNode(track_data[i].name))
+                    box.appendChild(sample_dropdown)
+                    box.appendChild(checkbox)
+                    backing_tracks.appendChild(box)
+                }
+            }
+        } else {
+            console.error('An error occurred fetching the track options')
+        }
+    }
+
+    request.send()
+}
 
 function startRecording() {
     const handleSuccess = function (stream) {
@@ -67,8 +119,6 @@ function finishRecording() {
 }
 
 function handleSubmit(event) {
-
-
     // Open POST request
     const request = new XMLHttpRequest();
     request.open("POST", "/api/upload_track/" + score_id + "/" + track_id);
@@ -91,4 +141,6 @@ function handleSubmit(event) {
 
 const form = document.querySelector('form');
 form.addEventListener('submit', handleSubmit);
+
+getSamplingTracks();
 
