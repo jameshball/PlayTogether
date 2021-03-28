7648d72e241c884127f9450d55f5bbd748b5fdf6
Jessica Lally <jml19@ic.ac.uk>
2021-03-28 00:36:07 +0000

    Fix merge conflicts and add dropdown for sampling tracks

--- static/record_track.js
+++ static/record_track.js
@@ -1,3 +1,37 @@
+let dropdown = document.getElementById('sampling-track-dropdown')
+dropdown.length = 0
+
+let defaultOption = document.createElement('option')
+defaultOption.text = 'Choose sampling track'
+
+dropdown.add(defaultOption)
+dropdown.selectedIndex = 0
+
+// Open GET request to get tracks for score
+const track_url = '/api/list_tracks/' + score_id
+const request = new XMLHttpRequest()
+request.responseType = "json";
+request.open('GET', track_url, true)
+
+request.onload = function () {
+    if (request.status === 200) {
+        const tracks_data = JSON.parse(request.responseText)
+        console.log(tracks_data)
+        let track
+
+        for (let i=0; i < tracks_data.length; i++) {
+            track = document.createElement('track')
+            track.text = tracks_data[i].name
+            track.value = tracks_data[i].track_id
+            dropdown.add(track)
+        }
+    } else {
+        console.error('An error occurred fetching the track options')
+    }
+}
+
+request.send()
+
 function startRecording() {
 
 }
